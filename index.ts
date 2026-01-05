2f4d50e1c79b1cdd59f958f562515c6ad1b270b9
fabrisdev <ezepuntocom@hotmail.com>
2026-01-05 16:14:17 -0300

    Started laying base

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import args from 'args'
+
+args.option('register', 'Register the app on your context menu')
+const flags = args.parse(process.argv)
+if(flags.register) {
+  register()
+  process.exit(0)
+}
+
+function register() {
+
+}
