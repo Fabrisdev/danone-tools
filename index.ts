8e493aaaa57d03e36e324459508a7cea1c71cfea
fabrisdev <ezepuntocom@hotmail.com>
2026-01-05 18:10:36 -0300

    came back to vanilla

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import args from 'args'
 import { $ } from 'bun'
-import { promisified as regedit } from 'regedit'
 
 args.option('register', 'Register the app on your context menu')
 args.option('unregister', "Removes the app from your context menu")
@@ -23,42 +22,16 @@ process.exit(1)
 async function register() {
 	const exe = process.execPath;
   const base = 'HKCU\\Software\\Classes'
-  const appName = 'DanoneTools'
 
-	await regedit.createKey([
-    `${base}\\.lin`,
-    `${base}\\${appName}.lin\\shell\\${appName}`,
-    `${base}\\${appName}.lin\\shell\\${appName}\\shell\\Decompile`,
-    `${base}\\${appName}.lin\\shell\\${appName}\\shell\\Decompile\\command`
-  ])
+	const menu = `${base}\\DanoneTools.lin\\shell\\DanoneTools`;
 
-  await regedit.putValue({
-    [`${base}\\.lin`]: {
-			'': { value: `${appName}.lin`, type: 'REG_SZ' }
-		},
-
-		[`${base}\\${appName}.lin\\shell\\${appName}`]: {
-			'': { value: appName, type: 'REG_SZ' },
-			SubCommands: { value: '', type: 'REG_SZ' }
-		},
-
-		[`${base}\\${appName}.lin\\shell\\${appName}\\shell\\Decompile`]: {
-			'': { value: 'Decompile', type: 'REG_SZ' }
-		},
-
-		[`${base}\\${appName}.lin\\shell\\${appName}\\shell\\Decompile\\command`]: {
-			'': {
-				value: `"${exe}" --decompile "%1"`,
-				type: 'REG_SZ'
-			}
-		}
-  })
-
-	console.log("DanoneTools registered succesfully.");
+	await $`reg add ${menu} /ve /d DanoneTools /f`;
+	await $`reg add ${menu} /v SubCommands /t REG_SZ /d "" /f`;
+	await $`reg add ${menu}\\shell\\Decompile /ve /d Decompile /f`;
+	await $`reg add ${menu}\\shell\\Decompile\\command /ve /d "\\"${exe}\\" --decompile \\"%1\\"" /f`;
 }
 
 async function unregister() {
   await $`reg delete HKCR\\DanoneTools.lin /f`;
-	await $`reg delete HKCR\\.lin /f`;
 	console.log("DanoneTools unregistered succesfully.");
 }
