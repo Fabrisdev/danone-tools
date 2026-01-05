74d722de0351c4981dd5c32e60b6ccb748c15d19
fabrisdev <ezepuntocom@hotmail.com>
2026-01-05 16:19:56 -0300

    Added message when no args passed

--- index.ts
+++ index.ts
@@ -2,11 +2,17 @@ import args from 'args'
 
 args.option('register', 'Register the app on your context menu')
 const flags = args.parse(process.argv)
+
 if(flags.register) {
   register()
   process.exit(0)
 }
 
+console.error('No options were supplied. Please check --help to see available commands.')
+process.exit(1)
+
 function register() {
 
+  console.log('Registered')
 }
+
