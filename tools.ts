60cb7ed3d53ab1b92c416111f94c87ce129bc00b
fabrisdev <ezepuntocom@hotmail.com>
2026-01-05 20:03:45 -0300

    Added simple write to see if context menu is working

--- tools.ts
+++ tools.ts
@@ -1,1 +1,11 @@
-export async function decompile(filePath: string) {}
+import path from "node:path";
+
+export async function decompile(filePath: string) {
+	const dir = path.dirname(filePath);
+	const name = path.basename(filePath, path.extname(filePath));
+	await Bun.write(
+		path.join(dir, `${name}.txt`),
+		`OK\nFile: ${filePath}\nTime: ${new Date().toISOString()}`,
+	);
+	console.log("File decompiled succesfully.");
+}
