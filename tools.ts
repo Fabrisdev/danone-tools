c42a0730fe6cb9f01183e26578d2f5b3ac7827bb
fabrisdev <ezepuntocom@hotmail.com>
2026-01-05 20:26:27 -0300

    Added compiling

--- tools.ts
+++ tools.ts
@@ -4,3 +4,8 @@ export async function decompile(filePath: string) {
 	await $`lin_compiler.exe -d ${filePath}`;
 	console.log("File decompiled succesfully.");
 }
+
+export async function compile(filePath: string) {
+	await $`lin_compiler.exe ${filePath}`;
+	console.log("File compiled succesfully.");
+}
